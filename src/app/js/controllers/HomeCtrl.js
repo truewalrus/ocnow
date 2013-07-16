376acb280c8bbe6ce751d44b8cfc32d616636d3d
jlevy <gorrum@gmail.com>
2013-07-16 14:13:21 -0700

    minor changes and tests, to the surprise of no one, something is wrong with twitter bootstrap and I'm too lazy to fix it currently

--- src/app/js/controllers/HomeCtrl.js
+++ src/app/js/controllers/HomeCtrl.js
@@ -7,4 +7,29 @@ angular.module("myApp.controllers").controller('HomeCtrl', ['$scope', function($
         $scope.cont = !$scope.cont;
     };
 
+
+    $scope.posts = [
+        {
+            img: "../img/testimg.jpg",
+            title: "20 JavaScript Frameworks Worth Checking Out",
+            submitter: "Siddharth",
+            date: "Sep 23rd 2011",
+            numberOfComments: "44",
+            content: "JavaScript testing is a sensitive subject. Some developers are huge advocates of it (including myself), while others don't see the need or benefit. One huge barrier is the simple fact that it can sometimes take a considerable amount of setup to get up and running. The longer it takes, the more likely it is that the developer simply won't bother. That's why Testem is so fantastic; it makes testing as effortless as possible, and, more importantly, fun!" +
+                "" +
+                "JavaScript testing is a sensitive subject. Some developers are huge advocates of it (including myself), while others don't see the need or benefit. One huge barrier is the simple fact that it can sometimes take a considerable amount of setup to get up and running. The longer it takes, the more likely it is that the developer simply won't bother. That's why Testem is so fantastic; it makes testing as effortless as possible, and, more importantly, fun!"
+        },
+        {
+            img: "../img/testimg.jpg",
+            title: "20 JavaScript Frameworks Worth Checking Out",
+            submitter: "Siddharth",
+            date: "Sep 23rd 2011",
+            numberOfComments: "44",
+            content: "JavaScript testing is a sensitive subject. Some developers are huge advocates of it (including myself), while others don't see the need or benefit. One huge barrier is the simple fact that it can sometimes take a considerable amount of setup to get up and running. The longer it takes, the more likely it is that the developer simply won't bother. That's why Testem is so fantastic; it makes testing as effortless as possible, and, more importantly, fun!" +
+                "" +
+                "JavaScript testing is a sensitive subject. Some developers are huge advocates of it (including myself), while others don't see the need or benefit. One huge barrier is the simple fact that it can sometimes take a considerable amount of setup to get up and running. The longer it takes, the more likely it is that the developer simply won't bother. That's why Testem is so fantastic; it makes testing as effortless as possible, and, more importantly, fun!"
+        }
+    ];
+
+
 }]);
