138cdfdf5f4e5f2765b017438449a8a51763bfd6
jlevy <gorrum@gmail.com>
2013-08-17 17:38:19 -0700

    cleanup

--- src/app/js/services/messageService.js
+++ src/app/js/services/messageService.js
@@ -5,14 +5,7 @@ angular.module('myApp.services')
         $rootScope.$on('MessagePopup', function(evt, msg_error, msg_notify) {
             $rootScope.msg_error = msg_error;
             $rootScope.msg_notify = msg_notify;
-            console.log('did i get in?');
-            console.log(msg_error);
-            console.log(msg_notify);
             $timeout(onMsgTimeout, 3000);
-        });
-
-        $rootScope.$on('TESTEMIT', function(evt){
-            console.log("DID IT WORK?");
         });
 
         var onMsgTimeout = function(){
