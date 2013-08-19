ed2238637bf63fcde35d07b54ab13387e0a473fb
your name <rekursiv@gmail.com>
2013-08-18 18:19:20 -0700

    Changes

--- src/app/js/filters/blurbFilter.js
+++ src/app/js/filters/blurbFilter.js
@@ -3,8 +3,37 @@ angular.module('myApp.filters')
     .filter('blurbFilter', [ function() {
         return function(text) {
             var blurb = text;
-            blurb = blurb.replace('</p>',' ');
-            blurb = blurb.replace(new RegExp('\\<.+?\\>','ig'), '');
-            return blurb;
+//            blurb = blurb.replace('</p>',' ');
+//            blurb = blurb.replace(new RegExp('\\<.+?\\>','ig'), '');
+
+            var links = true;
+
+            var matches = blurb.match(new RegExp('(https?://.\\S+)|(www\\.\\S+\\.\\S+)','ig'));
+
+            var linkedText = '';
+
+            if (matches) {
+                for (var i = 0; i < matches.length; i++) {
+                    var location = blurb.indexOf(matches[i]);
+
+                    if (matches[i].search('https?://') == -1) {
+                        linkedText = linkedText + blurb.substring(0, location) + '<a href="http://' + matches[i] + '">' + matches[i] + '</a>';
+                    }
+                    else {
+                        linkedText = linkedText + blurb.substring(0, location) + '<a href="' + matches[i] + '">' + matches[i] + '</a>';
+                    }
+
+                    blurb = blurb.substring(location + matches[i].length);
+
+                    console.log(matches[i]);
+                    console.log(location);
+                }
+            }
+
+            linkedText = linkedText + blurb;
+
+            linkedText = linkedText.replace(new RegExp('\n','g'), '<br />');
+
+            return linkedText;
         };
     }]);
