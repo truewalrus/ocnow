5f07b11f577593d0727eabbf807eb1ad51c7c460
your name <rekursiv@gmail.com>
2013-08-22 04:11:26 -0700

    Blurb changes

--- src/app/js/filters/blurbFilter.js
+++ src/app/js/filters/blurbFilter.js
@@ -3,34 +3,11 @@ angular.module('myApp.filters')
     .filter('blurbFilter', [ function() {
         return function(text) {
             var blurb = text;
-//            blurb = blurb.replace('</p>',' ');
-//            blurb = blurb.replace(new RegExp('\\<.+?\\>','ig'), '');
-
-            var links = true;
-
-            var matches = blurb.match(new RegExp('(https?://.\\S+)|(www\\.\\S+\\.\\S+)','ig'));
-
-            var linkedText = '';
-
-            if (matches) {
-                for (var i = 0; i < matches.length; i++) {
-                    var location = blurb.indexOf(matches[i]);
-
-                    if (matches[i].search('https?://') == -1) {
-                        linkedText = linkedText + blurb.substring(0, location) + '<a href="http://' + matches[i] + '">' + matches[i] + '</a>';
-                    }
-                    else {
-                        linkedText = linkedText + blurb.substring(0, location) + '<a href="' + matches[i] + '">' + matches[i] + '</a>';
-                    }
-
-                    blurb = blurb.substring(location + matches[i].length);
-                }
-            }
-
-            linkedText = linkedText + blurb;
-
-            linkedText = linkedText.replace(new RegExp('\n','g'), '<br />');
-
-            return linkedText;
+            blurb = blurb.replace('</p>',' ');
+            blurb = blurb.replace(new RegExp('\\<.+?\\>','ig'), '');
+            blurb = blurb.replace('&nbsp;', ' ');
+            blurb = blurb.replace('&lt;', '<');
+            blurb = blurb.replace('&gt;', '>');
+            return blurb;
         };
     }]);
