e57694dcbd4b17479882d4bfd0993c2563a64707
truewalrus <unbreakablescorpions@gmail.com>
2013-09-08 22:36:55 -0700

    some changes to database.js

--- src/database.js
+++ src/database.js
@@ -1,8 +1,14 @@
 var mongodb = require('mongodb'),
-	mongoserver = new mongodb.Server('localhost', mongodb.Connection.DEFAULT_PORT, {'auto-reconnect': true}),
-    ObjectID = require('mongodb').ObjectID,
+    ObjectID = require('mongodb').ObjectID
+
+if (process.env.MONGOLAB_URI){
+   console.log("AM I IN HERE IS THIS GONNA WORK LET IF YOU COULD LET MKE KNOW THAT'D BE GREAT");
+   db_connector = new mongodb.Db('ocnow', new mongodb.Server(process.env.MONGOLAB_URI, mongodb.Connection.DEFAULT_PORT, {'auto-reconnect': true}), {'safe':false, 'strict':true});
+}
+else{
+	var mongoserver = new mongodb.Server('localhost', mongodb.Connection.DEFAULT_PORT, {'auto-reconnect': true}),
 	db_connector = new mongodb.Db('test1', mongoserver, {'safe': false, 'strict': true});
-	
+}
 db_connector.open(function(err, db) {
 	for (var i = 0; i < collections.length; i++) {
 		collections[i](err, db);
